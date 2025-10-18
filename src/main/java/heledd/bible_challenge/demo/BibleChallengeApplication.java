d09e2b9e6b31145e5c4319c901adbe01e926fb98
heleledd <157613131+heleledd@users.noreply.github.com>
2025-10-18 11:11:50 +0100

    spring boot starter and added dependencies

---
+++ src/main/java/heledd/bible_challenge/demo/BibleChallengeApplication.java
@@ -0,0 +1,13 @@
+package heledd.bible_challenge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heledd/bible_challenge/demo/BibleChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package heledd.bible_challenge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
