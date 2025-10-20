962d3a22d72b55aa9680ad429a15f26d27f72375
heleledd <157613131+heleledd@users.noreply.github.com>
2025-10-20 09:46:10 +0100

    in the middle of doing the JWT token

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package heledd.bible_challenge.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BibleChallengeApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BibleChallengeApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package heledd.bible_challenge.demo;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BibleChallengeApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
